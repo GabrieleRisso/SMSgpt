5f0d607f9c88f7c34606415d837f2300dcd0daf8
exitcode0 <51493198+GabrieleRisso@users.noreply.github.com>
2023-07-06 03:10:05 +0200

    Update smsgpt.sh

--- smsgpt.sh
+++ smsgpt.sh
@@ -4,12 +4,12 @@
 # Tested on Arch-linux and Google Pixel 6 with Andorid 13
 
 
-#YOU MUST export open-ai api key
+#export open-ai api key
 #export OPENAI_API_KEY="sk-BQfU5xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 #for wireless adb
 #adb connect 192.168.1.55
-
+#or
 #for adb wired just connect your phone. must be in device mode
 #adb devices
 
@@ -25,15 +25,14 @@ while true; do
   
 
   #command to get the actual counter value
-
   if ((new_counter > counter)); then
 
-	#find the last recived message, and grep only the adress of it.
+	#find the last  message, and grep only the adress of it.
 	addr="$(adb shell 'content query --uri content://sms/inbox --projection date,address,body --sort "date ASC"' | sed '/^$/d' | tail -n 1 | awk '{print $4}' | cut -d'=' -f2- | sed 's/,$//')"
 	
  	echo "{+++} Message recived form $addr:"
 	
-	#find the last recived message, and grep only the body of it.
+	#find the last  message, and grep only the body of it.
 	body="$(adb shell 'content query --uri content://sms/inbox --projection date,address,body --sort "date ASC"' | sed '/^$/d' | tail -n 1 | grep -oP '(?<=body=).*' )"
 	
 	echo "{body}: $body"
@@ -46,11 +45,11 @@ while true; do
 	#replay with SMS
 	adb shell service call isms 5 i32 0 s16 "com.android.mms.service" s16 "null" s16 "${addr}" s16 "null" s16 ${rep} s16 "null" s16 "null" i32 0 i64 0
 
-    echo "{!!!} Sending message to $addr"
+    	echo "{!!!} Sending message to $addr"
 	#echo "C: $counter"
 
 	#update counters
-    counter=$new_counter
+    	counter=$new_counter
 
 	#remove this line if you wnant to use it with other GSM clients other then yourself 
 	counter=$((counter+1))
@@ -65,7 +64,7 @@ done
 
 
 #---------------
-#HACKY way of sending sms;
+#alternative way of sending sms;
 #adb shell am start -a android.intent.action.SENDTO -d sms:+39xxxxxxxxx --es sms_body "$txt" --ez exit_on_sent false;
 #sleep 1;
 #adb shell input keyevent 22 && adb shell input keyevent 22 && adb shell input keyevent 23;
