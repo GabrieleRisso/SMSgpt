585679b44138c549447b6a70f9dd5d5a3b5a4498
exitcode0 <51493198+GabrieleRisso@users.noreply.github.com>
2023-07-19 00:40:07 +0200

    Add files via upload

---
+++ sms.sh
@@ -0,0 +1,90 @@
+# Author: Gabriele Risso  License: MIT
+# SMSgpt: https://github.com/GabrieleRisso/SMSgpt/tree/main
+# POC of a phone used as SMS gateway to serve queries to chatGPT over GSM network using the regular Android message app.
+# Tested on Arch-linux and Google Pixel 6 with Andorid 13
+
+
+#export open-ai api key
+#export OPENAI_API_KEY="sk-BQfU5xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+#for wireless adb
+#adb connect 192.168.1.55
+#or
+#for adb wired just connect your phone. must be in device mode
+#adb devices
+
+
+echo "{OK} STARTED."
+
+
+counter=$(adb shell 'content query --uri content://sms/inbox --projection address,body --sort "date ASC"'  | sed '/^$/d' | tail -n 1 | awk '{print $2}' | cut -d'=' -f2- | sed 's/,$//')
+
+while true; do
+  new_counter=$(adb shell 'content query --uri content://sms/inbox --projection address,body --sort "date ASC"' | sed '/^$/d' | tail -n 1 | awk '{print $2}' | cut -d'=' -f2- | sed 's/,$//')  
+  #echo "L: $new_counter"
+  spin='-\|/'
+
+  for i in {1..10}; do
+    printf "\r%c Waiting for new incoming messages... " "${spin:i%${#spin}:1}"
+    sleep 0.1
+  done
+
+  #command to get the actual counter value
+  if ((new_counter > counter)); then
+	
+ 	      record="$(adb shell 'content query --uri content://sms/inbox --projection _id,date,address,body --sort "date ASC"' | sed '/^$/d' | tail -n 1 )"
+       	
+	#find the last message, and grep only the address of it.
+        addr="$(echo $record | awk '{print $5}' | cut -d'=' -f2- | sed 's/,$//')" 	
+        
+	#find the last received message, and grep only the body of it.
+        body="$(echo $record | grep -oP '(?<=body=).*' )" 
+
+ 	#disable loopback
+        if [ "$old_rep" != "$body" ]; then
+            
+            echo ""
+            echo "{<--} Incoming message N^$new_counter form $addr"
+            echo "{type}:		inbound"
+            echo "{body}:		$body"
+            echo ""
+            
+            #generate chatgpt reply
+            raw_rep="$(sgpt --chat "$addr-$(date +%Y-%m-%d)" "\"$body\" ")"
+            rep=$(echo $raw_rep | sed 's/ /\\ /g' | sed "s/'/\\\'/g" | sed 's/(/- -/; s/)/- -/g')
+            
+            #debug send
+            echo ""
+            echo "{-->} Sending message to $addr"
+            echo "{type}:		outbound"
+            echo "{body}:		$raw_rep"
+            echo ""
+            #adb send SMS to $addr with content $rep
+            #adb shell service call isms 5 i32 0 s16 "com.android.mms.service" s16 "null" s16 "${addr}" s16 "null" s16 ${rep} s16 "null" s16 "null" i32 0 i64 0 &
+            adb shell am start -a android.intent.action.SENDTO -d sms:"$addr" --es sms_body "${rep}" --ez exit_on_sent false;
+            sleep 0.5;
+            adb shell input keyevent 22 && adb shell input keyevent 22 && adb shell input keyevent 23;
+        else
+            echo "{XXX} Duplicate message loop, skipping"
+        fi
+
+	#save last reply for 
+ 	    old_rep=$raw_rep
+ 	#update counter
+    	counter=$new_counter
+
+	#remove this line if you want to use it with other GSM clients other then yourself safely
+	counter=$((counter+1))
+
+  fi
+
+  #sleep 1
+
+done
+
+
+#---------------
+#alternative way of sending sms;
+#adb shell am start -a android.intent.action.SENDTO -d sms:+39xxxxxxxxx --es sms_body "$txt" --ez exit_on_sent false;
+#sleep 1;
+#adb shell input keyevent 22 && adb shell input keyevent 22 && adb shell input keyevent 23;
